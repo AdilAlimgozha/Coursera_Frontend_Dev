080c4b025b8aab2b4702c03d2a26b049e7a4742c
AdilAlimgozha <alimgozha2003@gmail.com>
2023-11-10 20:53:55 +0600

    finished js logic, started css design

--- self/main.js
+++ self/main.js
@@ -58,11 +58,28 @@ function equals(){
 }
 
 function clean(){
-    result.innerText = `
+    result.innerText = `0
     `;
     n1 = false;
     n2 = false;
     action = false;
+}
+
+
+function change_sign(){
+    let temp_n2 = '0';
+    temp_n2 = (parseFloat(result.innerText) - 2 * parseFloat(result.innerText)).toString();
+    result.innerText = temp_n2;
+}
+
+function dectimal_num(){
+    if (result.innerText.includes('.') == false){
+        result.innerText += '.';   
+    }
+}
+
+function delete_num(){
+    result.innerText = result.innerText.slice(0, result.innerText.length - 1);
 }
 
 let n1 = false;
@@ -79,3 +96,6 @@ buttons[6].addEventListener('click', mult);
 buttons[10].addEventListener('click', minus);
 buttons[14].addEventListener('click', plus);
 buttons[18].addEventListener('click', equals);
+buttons[15].addEventListener('click', change_sign);
+buttons[17].addEventListener('click', dectimal_num);
+buttons[1].addEventListener('click', delete_num);
